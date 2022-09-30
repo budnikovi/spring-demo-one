539e7021080877601fc4baee75a99fac9b1d795f
Budnikov Illia <ilyabudkov@gmail.com>
2022-09-30 19:10:58 +0300

    adding a new project that I am using to train the spring framework

---
+++ src/main/java/MyApp.java
@@ -0,0 +1,8 @@
+public class MyApp {
+    public static void main(String[] args) {
+        //create the object
+        Coach theCoach = new BaseballCoach();
+        //use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
