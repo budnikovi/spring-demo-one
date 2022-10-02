417d5be1ffad96b6516fcc6c258a9789fab697f4
Budnikov Illia <ilyabudkov@gmail.com>
2022-10-02 19:38:05 +0300

    adding new files in this project, may be that is last commit in this project, but I'm not sure

--- src/main/java/run/BeanScopeDemoApp.java
+++ src/main/java/run/BeanScopeDemoApp.java
@@ -9,12 +9,23 @@ public class BeanScopeDemoApp {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 
+        System.out.println("Singleton scope: ");
+        footballCoachScope(context);
+
+        System.out.println("Prototype scope: ");
+        myCoachScope(context);
+
+        //close the context
+        context.close();
+    }
+    public static void myCoachScope(ClassPathXmlApplicationContext context) {
         //retrieve bean from spring container
         Coach theCoach = context.getBean("myCoach",Coach.class);
 
         Coach alphaCoach = context.getBean("myCoach",Coach.class);
 
         //check if they are the same
+
         boolean result = (theCoach == alphaCoach);
 
         // print out the results
@@ -24,7 +35,23 @@ public class BeanScopeDemoApp {
 
         System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
 
-        //close the context
-        context.close();
+    }
+    public static void footballCoachScope(ClassPathXmlApplicationContext context) {
+        //retrieve bean from spring container
+        Coach theCoach = context.getBean("footballCoach",Coach.class);
+
+        Coach alphaCoach = context.getBean("footballCoach",Coach.class);
+
+        //check if they are the same
+
+        boolean result = (theCoach == alphaCoach);
+
+        // print out the results
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location for theFootballCoach: " + theCoach);
+
+        System.out.println("\nMemory location for alphaFootballCoach: " + alphaCoach + "\n");
     }
 }
+
